2c06f8151d854f15edf325b140f14de38a47f796
djwjfk <grw07236@naver.com>
2023-05-06 02:23:18 +0900

    feat:calendar modal 제작

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,9 +1,35 @@
 import React, { useRef, useState, useEffect } from "react";
+import styled from "styled-components";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
-const Calendar = ({ localeDate }) => {
+const SModalBackground = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+`;
+const SCloseBtn = styled.div`
+  font-size: 30px;
+  text-align: right;
+  cursor: pointer;
+`;
+const SCalendarModal = styled.div`
+  width: 900px;
+  height: 90%;
+  border: 1px solid black;
+  margin: 30px;
+  padding: 0px 10px;
+  background-color: white;
+`;
+const SDateBook = styled.div`
+  font-size: 20px;
+`;
+
+const Calendar = ({ localeDate, showModal, handleCloseModal }) => {
   const calendarRef = useRef(null);
   const [calendarEvents, setCalendarEvents] = useState([]);
   const [data, setDate] = useState();
@@ -26,25 +52,35 @@ const Calendar = ({ localeDate }) => {
 
   return (
     <div>
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        initialView="dayGridMonth"
-        selectable={true} //사용자가 날짜를 선택할 수 있는지 여부
-        navLinks={true} // 탐색 링크를 표시할지 여부
-        editable={true} //이벤트를 편집할 수 있는지
-        nowIndicator={true} //현재시간 표시 여부
-        dayMaxEvents={true} //하루에 표시할 최대 이벤트 수 표시 여부
-        ref={calendarRef}
-        dateClick={handleDateClick}
-        events={calendarEvents}
-        eventAdd={handleEventAdd} //이벤트가 추가되면 발생하는 함수
-        // eventChange = {} // 이벤트가 수정되면 발생하는 함수
-        // eventRemove = {} // 이벤트가 삭제되면 발생하는 함수
-      />
       <div>
-        {/* || 연산자 : data 값이 없으면 넘어가서 다음꺼! */}
-        <p>선택한 날짜 : {data || localeDate}</p>
+        {showModal ? (
+          <SModalBackground onClick={handleCloseModal}>
+            <SCalendarModal>
+              <SCloseBtn onClick={handleCloseModal}>&times;</SCloseBtn>
+              <FullCalendar
+                plugins={[dayGridPlugin, interactionPlugin]}
+                initialView="dayGridMonth"
+                selectable={true} //사용자가 날짜를 선택할 수 있는지 여부
+                navLinks={true} // 탐색 링크를 표시할지 여부
+                editable={true} //이벤트를 편집할 수 있는지
+                nowIndicator={true} //현재시간 표시 여부
+                dayMaxEvents={true} //하루에 표시할 최대 이벤트 수 표시 여부
+                ref={calendarRef}
+                dateClick={handleDateClick}
+                events={calendarEvents}
+                eventAdd={handleEventAdd} //이벤트가 추가되면 발생하는 함수
+                // eventChange = {} // 이벤트가 수정되면 발생하는 함수
+                // eventRemove = {} // 이벤트가 삭제되면 발생하는 함수
+              />
+            </SCalendarModal>
+          </SModalBackground>
+        ) : null}
       </div>
+
+      <SDateBook>
+        {/* || 연산자 : data 값이 없으면 넘어가서 다음꺼! */}
+        <p>{data || localeDate}</p>
+      </SDateBook>
     </div>
   );
 };
--- src/page/DateNotes.js
+++ src/page/DateNotes.js
@@ -1,15 +1,47 @@
 import Calendar from "../components/Calendar";
+import styled from "styled-components";
+import { ReactComponent as CalendarImg } from "../image/CalendarImg.svg";
+import { useState } from "react";
 
+const SCalendarDate = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const SContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+`;
 const DateNotes = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
   const today = new Date();
   const year = today.getFullYear();
   const month = (today.getMonth() + 1).toString().padStart(2, "0");
   const day = today.getDate().toString().padStart(2, "0");
   const localeDate = `${year}-${month}-${day}`;
   return (
-    <div>
-      <Calendar localeDate={localeDate} />
-    </div>
+    <>
+      <SCalendarDate>
+        <Calendar
+          localeDate={localeDate}
+          showModal={showModal}
+          handleCloseModal={handleCloseModal}
+        />
+        <CalendarImg onClick={handleOpenModal} />
+      </SCalendarDate>
+      <SContentBox>
+        <input placeholder="제목을 입력하세요"></input>
+        <textarea placeholder="오늘의 데이트를 기록해보세요."></textarea>
+      </SContentBox>
+      <button>저장</button>
+    </>
   );
 };
 
